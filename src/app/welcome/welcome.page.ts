e7de671949879eda127c2b6718097e042587fa31
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-03 19:03:08 +0000

    feat (add) : Welcome page

---
+++ src/app/welcome/welcome.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.page.html',
+  styleUrls: ['./welcome.page.scss'],
+})
+export class WelcomePage implements OnInit {
+
+  
+
+  constructor(public navCtrl: NavController) { }
+
+  goToTab1() {
+    this.navCtrl.navigateRoot('tabs');
+  }
+
+  ngOnInit() {
+  }
+
+}
