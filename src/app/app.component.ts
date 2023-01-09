8d60b1a0b5032dc204e5f5edee3e0a8dd67baed1
Lucas Maciel <03lucasmaciel@gmail.com>
2023-01-09 12:00:54 +0000

    add : some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
     public navCtrl: NavController,
     private platform: Platform,
     private localizationService: LocalizationService) {
-      this.navCtrl.navigateRoot('tabs/tab1');
+      this.navCtrl.navigateRoot('tabs/tab4');
   }
 
   async ngOnInit() {
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Preferences } from '@capacitor/preferences';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
@@ -7,11 +8,14 @@ import { Preferences } from '@capacitor/preferences';
   styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
-
+  handlerMessage = '';
+  roleMessage = '';
   isChecked : boolean = false;
 
 
-  constructor() {}
+  constructor(
+    private alertController: AlertController
+  ) {}
 
   async ionViewWillEnter() {
     this.isChecked = (await Preferences.get({ key: 'darkmode' })).value === 'true';
@@ -28,6 +32,68 @@ export class Tab4Page {
       document.body.removeAttribute('color-theme');
     }
   }
+  
+  
+    async presentAlert() {
+      const alert = await this.alertController.create({
+        header: 'Jogo do Galo',
+        subHeader: 'Este jogo foi desenvolvido por mim, Lucas Maciel, e foi desenvolvido no âmbito de melhorar as minhas qualidades nas tecnologias de HTML, CSS e JavaScript',
+        buttons: [
+          {
+            text: 'OK',
+            role: 'confirm',
+            handler: () => {
+              this.handlerMessage = 'Alert confirmed';
+            },
+          },
+        ],
+      });
+  
+      await alert.present();
+  
+      const { role } = await alert.onDidDismiss();
+      this.roleMessage = `Dismissed with role: ${role}`;
+    }
 
+    async presentAlert2() {
+      const alert2 = await this.alertController.create({
+        header: 'Video Club',
+        subHeader: 'Este projeto foi desenvolvido no âmbito da discplina de Aplicações Web, com o objeto de construir uma plataforma para aluguer de filmes.',
+        buttons: [
+          {
+            text: 'OK',
+            role: 'confirm',
+            handler: () => {
+              this.handlerMessage = 'Alert confirmed';
+            },
+          },
+        ],
+      });
+  
+      await alert2.present();
+  
+      const { role } = await alert2.onDidDismiss();
+      this.roleMessage = `Dismissed with role: ${role}`;
+    }
+    async presentAlert3() {
+      const alert3 = await this.alertController.create({
+        header: 'Management Company - PAP',
+        subHeader: 'Este foi o projeto final do meu curso profissional Técnico Informática de Gestão, que tem como principal objetivo fazer gestão dos balanços da empresa, e apresentar possíveis soluções para melhorias da empresa.',
+        buttons: [
+          {
+            text: 'OK',
+            role: 'confirm',
+            handler: () => {
+              this.handlerMessage = 'Alert confirmed';
+            },
+          },
+        ],
+      });
+  
+      await alert3.present();
+  
+      const { role } = await alert3.onDidDismiss();
+      this.roleMessage = `Dismissed with role: ${role}`;
+    }
 
 }
